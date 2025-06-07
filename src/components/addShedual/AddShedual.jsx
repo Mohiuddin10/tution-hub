2ccdb943cfc00e90638ab09c3dc5ed573a37057c
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-06-08 01:43:15 +0600

    shedual list

--- src/components/addShedual/AddShedual.jsx
+++ src/components/addShedual/AddShedual.jsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+
 const AddShedual = ({ shedual }) => {
+  const [shedualList, setShedualList] = useState([]);
+  useEffect(() => {
+    fetch("blogs.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const filteredShedual = data.find((item) => item.id === shedual);
+        console.log(filteredShedual);
+        setShedualList(shedualList.push(filteredShedual));
+      });
+  }, [shedual]);
+  console.log(shedualList);
   console.log(shedual);
   return <div>{shedual}</div>;
 };
